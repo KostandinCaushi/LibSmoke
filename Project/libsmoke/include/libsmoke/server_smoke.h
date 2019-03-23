b2c20477a3e74a0c52c3269f8f7fb97915bb3080
filippocollini <filippo_collini@yahoo.it>
2019-03-23 15:42:44 +0100

    [libsmoke] adding header file server_smoke.h

---
+++ Project/libsmoke/include/libsmoke/server_smoke.h
@@ -0,0 +1,18 @@
+//
+// Created by filippocollini on 3/23/19.
+//
+
+#ifndef SKNX_SERVER_SMOKE_H
+#define SKNX_SERVER_SMOKE_H
+
+class ServerSmoke{
+public:
+    ServerSmoke(){}
+
+    bool init(){
+        //setup the TCP connection
+    }
+
+};
+
+#endif //SKNX_SERVER_SMOKE_H
