c2b11b0059d19d9939e54361484723f86d5cbf30
Kostandin Caushi <koci4@live.it>
2019-03-27 20:51:34 +0100

    [client_test] initialized ClientSmoke

--- Project/libsmoke/tests/client_test.cpp
+++ Project/libsmoke/tests/client_test.cpp
@@ -1,1 +1,22 @@
 #include <libsmoke_client.h>
+#include <shared/knx/debug.h>
+#include <cstdlib>
+#include <shared/knx/crypto/mka.h>
+
+#include "connection_data.h"
+
+TAG_DEF("Main")
+
+int main() //int argc, char *argv[])
+{
+    KNX::ClientSmoke<KNX::MKAKeyExchange, TCP_PORT> client;
+
+    if(!client.init(2, TCP_IP)) {
+        printf("Cannot init TCP connection.");
+        exit(-1);
+    }
+
+    // TODO: send pkts
+
+    return 0;
+}
