0f39bb01d4d146fe88ffc9da2a207cfc2e12b866
Kostandin Caushi <koci4@live.it>
2019-03-27 20:52:12 +0100

    [libsmoke_client.h] updated methods and variables

--- Project/libsmoke/include/libsmoke/libsmoke_client.h
+++ Project/libsmoke/include/libsmoke/libsmoke_client.h
@@ -1,41 +1,36 @@
 #ifndef LIBSMOKE_CLIENT_H
 #define LIBSMOKE_CLIENT_H
 
-#include "common.h"
-#include <signal.h>
-#include <stdio.h>
-#include <knx/knx.h>
-#include <knx/backend/linux-tcp.h>
-#include <knx/crypto/mka.h>
-#include <knx/crypto/bd1.h>
-#include <knx/crypto/bd2.h>
-#include <knx/crypto/gdh2.h>
-#include <knx/nodecounter/dummy.h>
-#include <knx/pktwrapper.h>
-#include <knx/crypto/policies.h>
-#include <iostream>
-#include <knx/telegram.h>
 #include <aes.hpp>
+#include <telegram.h>
+#include "backend.h"
 
-class ClientSmoke {
-public:
-    ClientSmoke (){}
+static volatile bool _stopevent = false;
+static volatile uint8_t iv[16]  = { 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6,
+                                    0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
 
-    bool init() {
+namespace KNX {
 
-    }
+//TODO: comment
+    template<typename KeyAlgorithm, uint16_t PORT>
+    class ClientSmoke {
+    public:
+        ClientSmoke();
 
-    bool receive() {
+        //TODO: comment
+        bool init(int numClients, const char *addr);
 
-    }
+        //TODO: comment
+        bool receive(telegram &data);
 
-    bool send() {
+        //TODO: comment
+        void send(telegram &data);
 
-    }
-
-private:
-
-
-};
+    private:
+        KeyAlgorithm _key;
+        Backend &_backend;
+        struct AES_ctx ctx;
+    };
+} // end namespace KNX
 
 #endif //LIBSMOKE_CLIENT_H
