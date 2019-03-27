42d76f05a3dedd1daef1df5991b2b0d7e722c91c
filippocollini <filippo_collini@yahoo.it>
2019-03-27 11:19:20 +0100

    [libsmoke_server.cpp] implementing functions from header file libsmoke_server.h

--- Project/libsmoke/src/libsmoke_server.cpp
+++ Project/libsmoke/src/libsmoke_server.cpp
@@ -1,2 +1,32 @@
 #include <libsmoke_server.h>
+#include <knx/common.h>
+#include <common.h>
 
+#define ADDR "127.0.0.1"
+//defining #define PORT 123 unsigned int --> arises problems passing parameters in init
+//other problems if we try to use the TCP_PORT in common.h since it's declared as an int
+
+using namespace std;
+TAG_DEF("Main")
+
+int main(){
+    ServerSmoke server;
+    unsigned short port = 123;
+
+    if(!server.init(ADDR, port)){
+        LOG("Cannot init TCP connection.");
+        exit(-1);
+    }
+
+    printf("[MAIN] Server is up and running\n");
+    while(!mustStop)
+        server.run();
+
+    printf("[MAIN] Shutting down...\n");
+
+    server.shutdown();
+
+    printf("[MAIN] Goodbye!\n");
+
+    return 0;
+}
