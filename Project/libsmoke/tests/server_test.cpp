2e4d60dd7b2100bcf1c47ad04ecb8847740e84f4
Kostandin Caushi <koci4@live.it>
2019-03-27 17:01:42 +0100

    [server_test.cpp] created a test for server side
    inside has a main that creates a ServerSmoke and uses it

---
+++ Project/libsmoke/tests/server_test.cpp
@@ -0,0 +1,25 @@
+#include <libsmoke_server.h>
+
+#include "connection_data.h"
+
+TAG_DEF("Main")
+
+int main() //int argc, char *argv[])
+{
+    ServerSmoke server;
+
+    if(!server.init(TCP_IP, TCP_PORT)) {
+        printf("Cannot init TCP connection.");
+        exit(-1);
+    }
+
+    printf("[MAIN] Server is up and running\n");
+    while(!mustStop) {
+        server.run();
+    }
+
+    printf("[MAIN] Shutting down.. \n");
+    server.shutdown();
+    printf("[MAIN] Goodbye\n");
+    return 0;
+}
