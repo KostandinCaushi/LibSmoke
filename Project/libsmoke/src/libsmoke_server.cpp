9b1abc7a557111b98418eb382256ecabcf45c13c
Kostandin Caushi <koci4@live.it>
2019-03-27 17:05:01 +0100

    [libsmoke_server.cpp] implemented init() method

--- Project/libsmoke/src/libsmoke_server.cpp
+++ Project/libsmoke/src/libsmoke_server.cpp
@@ -1,6 +1,43 @@
 #include <libsmoke_server.h>
-#include <knx/common.h>
-#include <common.h>
+
+#define BUFSZ 512
+
+
+//TODO: comment
+bool ServerSmoke::init(const char *addr, int port) {
+    if(_ready)
+        return false;
+
+    struct sockaddr_in in;
+    in.sin_family = AF_INET;
+    in.sin_port = htons(port);
+    in.sin_addr.s_addr = inet_addr(addr);
+
+    _sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(_sock < 0) {
+        fprintf(stderr, "Cannot create server socket\n");
+        _ready = false;
+        return false;
+    }
+
+    if(bind(_sock, (struct sockaddr *)&in, sizeof(in)) < 0) {
+        fprintf(stderr, "Cannot bind on port %u\n", port);
+        close(_sock);
+        _ready = false;
+        return false;
+    }
+
+    if(listen(_sock, 10) < 0) {
+        fprintf(stderr, "Cannot start listening\n");
+        close(_sock);
+        _ready = false;
+        return false;
+    }
+
+    _ready = true;
+    return true;
+}
+
 
 #define ADDR "127.0.0.1"
 //defining #define PORT 123 unsigned int --> arises problems passing parameters in init
