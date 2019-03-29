d434e2dcba41824565c2a60aafbb57f970c24d17
Kostandin Caushi <koci4@live.it>
2019-03-30 00:56:57 +0100

    [client_test] added a pkt (sent & received) to test libsmoke

--- Project/libsmoke/tests/client_test.cpp
+++ Project/libsmoke/tests/client_test.cpp
@@ -13,7 +13,24 @@ int main() //int argc, char *argv[])
         exit(-1);
     }
 
-    // TODO: send pkts
+    sleep(2);
+
+    // Test to send msg broadcast
+    KNX::telegram pkt;
+    uint8_t in[15]  = { 0x60, 0x1e, 0xc3, 0x13, 0x77, 0x57, 0x89, 0xa5, 0xb7, 0xa7, 0xf5, 0x04, 0xbb, 0xf3, 0xd2 };
+    if(pkt.write(in, 15)) {
+        client.send(pkt);
+    }
+
+    // Get sent pkt
+    KNX::telegram pkt2;
+    if (!client.receive(pkt2)) {
+        printf("\nNo pkt received !!\n");
+    }
+    KNX::telegram pkt3;
+    if (!client.receive(pkt3)) {
+        printf("\nNo pkt received !!\n");
+    }
 
     return 0;
 }
