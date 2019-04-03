65b6a530c246ee4e9efa264a976ca9918f9bc86b
Kostandin Caushi <koci4@live.it>
2019-04-03 23:29:02 +0200

    [libsmoke_client] added the use of pktwrapper in order to receive pkts
    intead of using directly _backend, we can now call pktwrapper and receive larger pkts

--- Project/libsmoke/src/libsmoke_client.h
+++ Project/libsmoke/src/libsmoke_client.h
@@ -66,17 +66,20 @@ public:
 
 
     //TODO: comment
-    bool receive(KNX::telegram &data) {
-        _backend.update();
-        if (_backend.read(data)) {
+    bool receive(KNX::pkt_t &data) {
+        _pktwrapper.update();
+        if (_pktwrapper.read(data)) {
             printf("\nReceived packet\n");
-            Debug::printArray(data.body(), 15);
+            Debug::printArray(data.getData().data(), data.getData().size());
 
+            // Get data tmp parameters
+            uint8_t *tmpBuff = data.getData().data();
+            uint32_t buffSize = (uint32_t) data.getData().size();
             // Decrypt MSG
             AES_init_ctx_iv(&_ctx, _key.key(), iv);
-            AES_CTR_xcrypt_buffer(&_ctx, data.body(), 15);
+            AES_CTR_xcrypt_buffer(&_ctx, tmpBuff, buffSize);
             printf("\nDecrypted\n");
-            Debug::printArray(data.body(), 15);
+            Debug::printArray(tmpBuff, buffSize);
             return true;
         } else {
             return false;
