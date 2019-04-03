30aa48ab470bc7338b5d16ba167772e4d4dfa641
Kostandin Caushi <koci4@live.it>
2019-04-03 23:33:18 +0200

    [client_test] fixed in order to test updates

--- Project/libsmoke/tests/client_test.cpp
+++ Project/libsmoke/tests/client_test.cpp
@@ -6,7 +6,7 @@ TAG_DEF("Main")
 
 int main() //int argc, char *argv[])
 {
-    ClientSmoke<KNX::MKAKeyExchange, TCP_PORT> client(CLIENTS_NUM, TCP_IP);
+    ClientSmoke<KNX::MKAKeyExchange, TCP_PORT, CLIENTS_NUM> client(TCP_IP);
 
     if(!client.init()) {
         printf("Cannot init TCP connection.");
@@ -16,20 +16,18 @@ int main() //int argc, char *argv[])
     sleep(2);
 
     // Test to send msg broadcast
-    KNX::telegram pkt;
-    uint8_t in[15]  = { 0x60, 0x1e, 0xc3, 0x13, 0x77, 0x57, 0x89, 0xa5, 0xb7, 0xa7, 0xf5, 0x04, 0xbb, 0xf3, 0xd2 };
-    if(pkt.write(in, 15)) {
-        client.send(pkt);
-    }
+    uint8_t in[20]  = { 0x60, 0x1e, 0xc3, 0x13, 0x77, 0x57, 0x89, 0xa5, 0xb7, 0xa7, 0xf5, 0x04, 0xbb, 0xf3, 0xd2,
+                    0x60, 0x1e, 0xc3, 0x13, 0x77 };
+
+    // Send data in broadcast, dest = 0x00, CMD_PING = 0x00
+    client.send(0x00, 0x00, in, 20);
 
     // Get sent pkt
-    KNX::telegram pkt2;
-    if (!client.receive(pkt2)) {
-        printf("\nNo pkt received !!\n");
-    }
-    KNX::telegram pkt3;
-    if (!client.receive(pkt3)) {
-        printf("\nNo pkt received !!\n");
+    while (true) {
+        KNX::pkt_t pkt2;
+        if (client.receive(pkt2)) {
+            break;
+        }
     }
 
     return 0;
